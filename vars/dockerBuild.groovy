4d960611cb8a7be4671957fc2ab328615c0cc48c
SonuChristo <sonuchristo455@gmail.com>
2023-10-16 23:49:13 +0530

    dockerImagePush dockerhub

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -5,3 +5,13 @@ def call(String project , String ImageTag , String hubUser){
         docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
     """
 }
+
+
+
+// def call(String aws_account_id , String  , String hubUser){
+//     sh """
+//         docker build -t ${hubUser}/${project} .
+//         docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+//         docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+//     """
+// }
