e43705048da6610b43659735f566953b0b5e1f56
SonuChristo <sonuchristo455@gmail.com>
2023-10-17 21:04:44 +0530

    dockerImageEcr push

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -28,3 +28,4 @@ def call(String aws_account_id, String region, String ecr_repo_name) {
         sh "docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repo_name}:latest"
     }
 }
+
