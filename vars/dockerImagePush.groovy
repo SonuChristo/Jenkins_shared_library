099e5fc5bc394c65b7ab0278656c8257a226137d
SonuChristo <sonuchristo455@gmail.com>
2023-10-17 12:43:50 +0530

    dockerImageEcr push

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,13 +1,13 @@
-def call(String project , String ImageTag , String hubUser){
+// def call(String project , String ImageTag , String hubUser){
     
         
-        withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            sh "docker login -u '$USER' -p '$PASS' "
-}
-      sh " docker image push  ${hubUser}/${project}:${ImageTag}"
-      sh " docker image push ${hubUser}/${project}:latest"
+//         withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+//             sh "docker login -u '$USER' -p '$PASS' "
+// }
+//       sh " docker image push  ${hubUser}/${project}:${ImageTag}"
+//       sh " docker image push ${hubUser}/${project}:latest"
     
-}
+// }
 
 def call(String aws_account_id , String region , String ecr_repo_name){
     sh """
--- vars/dockerImageScanner.groovy
+++ vars/dockerImageScanner.groovy
@@ -1,10 +1,10 @@
 //Jenkinsfile
-def call(String project , String ImageTag , String hubUser){
-    sh """
-        trivy image ${hubUser}/${project}:latest > scan.txt
-        cat scan.txt
-    """
-}
+// def call(String project , String ImageTag , String hubUser){
+//     sh """
+//         trivy image ${hubUser}/${project}:latest > scan.txt
+//         cat scan.txt
+//     """
+// }
 
 //jenkinsfile-ecr
 
