a59c3dfc001bcd998af9a66a5e5f977c7a18cc8c
SonuChristo <sonuchristo455@gmail.com>
2023-10-17 12:41:24 +0530

    dockerImageEcr push

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -8,3 +8,12 @@ def call(String project , String ImageTag , String hubUser){
       sh " docker image push ${hubUser}/${project}:latest"
     
 }
+
+def call(String aws_account_id , String region , String ecr_repo_name){
+    sh """
+        # Build your Docker image
+aws ecr get-login-password --region ${aws_region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${aws_region}.amazonaws.com
+# Tag the image with the ECR repository URL
+docker push ${aws_account_id}.dkr.ecr.${aws_region}.amazonaws.com/${ecr_repo_name}:latest
+    """
+}
--- vars/dockerImageScanner.groovy
+++ vars/dockerImageScanner.groovy
@@ -1,6 +1,30 @@
+//Jenkinsfile
 def call(String project , String ImageTag , String hubUser){
     sh """
         trivy image ${hubUser}/${project}:latest > scan.txt
         cat scan.txt
     """
 }
+
+//jenkinsfile-ecr
+
+def call(String aws_account_id , String region , String ecr_repo_name){
+    sh """
+        trivy image ${aws_account_id}.dkr.ecr.${aws_region}.amazonaws.com/${ecr_repo_name}:latest > scan.txt
+        cat scan.txt
+    """
+}
+
+
+
+// def call(String aws_account_id , String region , String ecr_repo_name){
+//     sh """
+//         # Build your Docker image
+// docker build -t ${ecr_repo_name} .
+
+// # Tag the image with the ECR repository URL
+// docker tag ${ecr_repo_name}:latest ${aws_account_id}.dkr.ecr.${aws_region}.amazonaws.com/${ecr_repo_name}:latest
+
+
+//     """
+// }
