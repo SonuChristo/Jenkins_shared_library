f3e7952b5f90f33156f738b00484532a957b043c
SonuChristo <sonuchristo455@gmail.com>
2023-10-19 00:26:52 +0530

    dockerImageEcr push

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -14,7 +14,7 @@ def call(String aws_account_id, String region, String ecr_repo_name) {
         $class: 'AmazonWebServicesCredentialsBinding',
         accessKeyVariable: 'AWS_ACCESS_KEY_ID',
         secretKeyVariable: 'AWS_SECRET_ACCESS_KEY',
-        credentialsId: 'YourAWSJenkinsCredentialID' // Replace with your Credential ID
+        credentialsId: 'ECR_Auth' // Use your Credential ID, which is 'ECR_Auth'
     ]]) {
         // Authenticate Docker with AWS
         sh "aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repo_name}"
@@ -23,6 +23,7 @@ def call(String aws_account_id, String region, String ecr_repo_name) {
         sh "docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repo_name}:latest"
     }
 }
+
 
 
 // Example usage:
